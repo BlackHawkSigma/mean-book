19064d8e18302a7f08818748441f575d790a1764
Tobias Beese <tobias.beese@gmx.de>
2016-05-11 21:02:15 +0200

    Change files to work

--- config/express.js
+++ config/express.js
@@ -1,7 +1,7 @@
 var express = require('express');
 
 module.exports = function() {
-  var = app.express();
+  var app = express();
   require('../app/routes/index.server.routes.js')(app);
   return app;
 };
