981a8c605a73494e7d80e1dd1141b454878ed899
Tobias Beese <tobias.beese@gmx.de>
2016-07-18 22:14:57 +0200

    Change console.log after server is ready

--- server.js
+++ server.js
@@ -9,7 +9,11 @@ var app = express();
 const hostname = 'localhost';
 const port = 3000;
 
-app.listen(port, hostname);
+var server = app.listen(port, hostname, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Express is up and running at http://%s:%s/', host, port);
+});
 module.exports = app;
 
-console.log('Server running at http://${hostname}:${port}/');
+// console.log('Server running at http://' + hostname + ':' + port + '/');
